458dad7742a7ea7ec1b62115de96c7ec3dd704c1
purnimadarsi <purnima.darsi@gmail.com>
2020-07-27 17:48:39 -0700

    final commit by asian development team

---
+++ src/test/java/pages/HelloWorld2.java
@@ -0,0 +1,14 @@
+package com.google.tests;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld2 {
+	
+	
+	
+	@Test
+	public void printing() {
+		System.out.println("Hello World2");
+	}
+
+}
